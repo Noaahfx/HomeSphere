992ce5f3ddc4aff2f7e102c7f6780be9808903a3
Noaahfx <soodinaynoah@gmail.com>
2025-02-13 13:29:12 +0800

    Test

--- HomeSphere/Form1.Designer.cs
+++ HomeSphere/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // directorySearcher1
@@ -38,20 +37,11 @@
             this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
             this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(195, 110);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(536, 152);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1002, 445);
-            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -61,7 +51,6 @@
         #endregion
 
         private System.DirectoryServices.DirectorySearcher directorySearcher1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
